0d068212e0a18bd9b4d97955d8e6b1fc2fa948c8
Keyur Patel <simkeyur@gmail.com>
2014-04-05 12:15:47 -0500

    redesigned Admin Panel

---
+++ admin/includes/navbar.php
@@ -0,0 +1,29 @@
+
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          
+          <a class="navbar-brand"  class="fa fa-globe" href="index.php">Sign Me Ups</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+		
+			
+			<ul class="nav navbar-nav navbar-right navbar-user">
+           
+           
+			
+            <li class="dropdown user-dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> Admin <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#"><i class="fa fa-gear"></i> Settings</a></li>
+                <li class="divider"></li>
+                <li><a href="#"><i class="fa fa-power-off"></i> Log Out</a></li>
+              </ul>
+            </li>
+          </ul>
+		  
+	
+        </div><!-- /.navbar-collapse -->
+      </nav>
---
+++ admin/index.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Sign Me Ups - Dashboard</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Add custom CSS here -->
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+	
+  </head>
+
+  <body>
+
+    <div>
+
+      <!-- Sidebar -->
+		<?php
+		//navigation
+		include_once('includes/navbar.php');
+		?>
+
+      <div id="page-wrapper">
+
+        <div class="row">
+          <div class="col-lg-12">
+            <h1>Forms <small>Overview</small></h1>
+            <ol class="breadcrumb">
+              <li class="active"><i class="fa fa-dashboard"></i> Dashboard</li>
+            </ol>
+            
+          </div>
+        </div><!-- /.row -->
+
+        <div class="row">
+		
+		  
+		  <!--Panel start-->
+          <div class="col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2 class="text-center"> Form-1</h2>
+                </div>
+                <div class="panel-body text-center">
+                    <div class="progress progress-default progress-striped active">
+						  <div class="bar" style="width: 40%;"></div>
+					</div>
+                </div>
+                <table class="table">
+					<tr><td><h5><b>Total Sign Ups: </b></h5></td><td>42</td></tr>
+					<tr><td><h5><b>Capacity: </b></h5></td><td>110</td></tr>
+					<tr><td><h5><b>Days Left: </b></h5></td><td>6</td></tr>
+				</table>
+				<center>
+                <div class="panel-footer">
+                    <a class="btn btn-success " href="#">Edit</a>
+					<a class="btn btn-danger " href="#">Delete</a>
+                </div>
+				<center/>
+            </div>
+        </div>
+		  <!--Panel END-->
+		  
+		   <!--Panel start-->
+          <div class="col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2 class="text-center"> Form-1</h2>
+                </div>
+                <div class="panel-body text-center">
+                    <div class="progress progress-default progress-striped active">
+						  <div class="bar" style="width: 40%;"></div>
+					</div>
+                </div>
+                <table class="table">
+					<tr><td><h5><b>Total Sign Ups: </b></h5></td><td>42</td></tr>
+					<tr><td><h5><b>Capacity: </b></h5></td><td>110</td></tr>
+					<tr><td><h5><b>Days Left: </b></h5></td><td>6</td></tr>
+				</table>
+				<center>
+                <div class="panel-footer">
+                    <a class="btn btn-success " href="#">Edit</a>
+					<a class="btn btn-danger " href="#">Delete</a>
+                </div>
+				<center/>
+            </div>
+        </div>
+		  <!--Panel END-->
+		  
+		   <!--Panel start-->
+          <div class="col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2 class="text-center"> Form-1</h2>
+                </div>
+                <div class="panel-body text-center">
+                    <div class="progress progress-default progress-striped active">
+						  <div class="bar" style="width: 40%;"></div>
+					</div>
+                </div>
+                <table class="table">
+					<tr><td><h5><b>Total Sign Ups: </b></h5></td><td>42</td></tr>
+					<tr><td><h5><b>Capacity: </b></h5></td><td>110</td></tr>
+					<tr><td><h5><b>Days Left: </b></h5></td><td>6</td></tr>
+				</table>
+				<center>
+                <div class="panel-footer">
+                    <a class="btn btn-success " href="#">Edit</a>
+					<a class="btn btn-danger " href="#">Delete</a>
+                </div>
+				<center/>
+            </div>
+        </div>
+		  <!--Panel END-->
+		  
+		   <!--Panel start-->
+          <div class="col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2 class="text-center"> Form-1</h2>
+                </div>
+                <div class="panel-body text-center">
+                    <div class="progress progress-default progress-striped active">
+						  <div class="bar" style="width: 40%;"></div>
+					</div>
+                </div>
+                <table class="table">
+					<tr><td><h5><b>Total Sign Ups: </b></h5></td><td>42</td></tr>
+					<tr><td><h5><b>Capacity: </b></h5></td><td>110</td></tr>
+					<tr><td><h5><b>Days Left: </b></h5></td><td>6</td></tr>
+				</table>
+				<center>
+                <div class="panel-footer">
+                    <a class="btn btn-success " href="#">Edit</a>
+					<a class="btn btn-danger " href="#">Delete</a>
+                </div>
+				<center/>
+            </div>
+        </div>
+		  <!--Panel END-->
+		  
+		   <!--Panel start-->
+          <div class="col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2 class="text-center"> Form-1</h2>
+                </div>
+                <div class="panel-body text-center">
+                    <div class="progress progress-default progress-striped active">
+						  <div class="bar" style="width: 40%;"></div>
+					</div>
+                </div>
+                <table class="table">
+					<tr><td><h5><b>Total Sign Ups: </b></h5></td><td>42</td></tr>
+					<tr><td><h5><b>Capacity: </b></h5></td><td>110</td></tr>
+					<tr><td><h5><b>Days Left: </b></h5></td><td>6</td></tr>
+				</table>
+				<center>
+                <div class="panel-footer">
+                    <a class="btn btn-success " href="#">Edit</a>
+					<a class="btn btn-danger " href="#">Delete</a>
+                </div>
+				<center/>
+            </div>
+        </div>
+		  <!--Panel END-->
+		  
+		   <!--Panel start-->
+          <div class="col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2 class="text-center"> Form-1</h2>
+                </div>
+                <div class="panel-body text-center">
+                    <div class="progress progress-default progress-striped active">
+						  <div class="bar" style="width: 40%;"></div>
+					</div>
+                </div>
+                <table class="table">
+					<tr><td><h5><b>Total Sign Ups: </b></h5></td><td>42</td></tr>
+					<tr><td><h5><b>Capacity: </b></h5></td><td>110</td></tr>
+					<tr><td><h5><b>Days Left: </b></h5></td><td>6</td></tr>
+				</table>
+				<center>
+                <div class="panel-footer">
+                    <a class="btn btn-success " href="#">Edit</a>
+					<a class="btn btn-danger " href="#">Delete</a>
+                </div>
+				<center/>
+            </div>
+        </div>
+		  <!--Panel END-->
+		  
+		  <!--Panel start-->
+          <div class="col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2 class="text-center"> Form-1</h2>
+                </div>
+                <div class="panel-body text-center">
+                    <div class="progress progress-default progress-striped active">
+						  <div class="bar" style="width: 40%;"></div>
+					</div>
+                </div>
+                <table class="table">
+					<tr><td><h5><b>Total Sign Ups: </b></h5></td><td>42</td></tr>
+					<tr><td><h5><b>Capacity: </b></h5></td><td>110</td></tr>
+					<tr><td><h5><b>Days Left: </b></h5></td><td>6</td></tr>
+				</table>
+				<center>
+                <div class="panel-footer">
+                    <a class="btn btn-success " href="#">Edit</a>
+					<a class="btn btn-danger " href="#">Delete</a>
+                </div>
+				<center/>
+            </div>
+        </div>
+		  <!--Panel END-->
+		  
+		  <!--Panel start-->
+		   <div class="col-md-3">
+				<a href="form.php"><img src="img/add.png" alt="some_text"></a>
+			</div>
+		  <!--Panel END-->
+		  
+		  
+		  
+		  
+        </div><!-- /.row -->
+		
+		
+
+
+         
+          
+		  
+
+      </div><!-- /#page-wrapper -->
+
+    </div><!-- /#wrapper -->
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <!-- Page Specific Plugins -->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+    <script src="http://cdn.oesmith.co.uk/morris-0.4.3.min.js"></script>
+    <script src="js/morris/chart-data-morris.js"></script>
+    <script src="js/tablesorter/jquery.tablesorter.js"></script>
+    <script src="js/tablesorter/tables.js"></script>
+	<script>
+	/* card flip */
+	$(".flip").hover(function(){
+	  $(this).find(".card").toggleClass("flipped");
+	  return false;
+	});
+	</script>
+  </body>
+  <hr>
+  <!-- start: Footer -->
+			<div id="footer">
+			<p>
+				<span class="hidden-phone" style="float:right">Powered by: <a href="http://idappthat.mobi" alt="UTA MOBI">UTA MOBI</a></span>
+			</p>
+		</div>
+			<!-- end: Footer -->
+</html>
